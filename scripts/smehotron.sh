05b7089fc17a073019e3400d434143d3c8d82e4f
WellRedPandit <boris.dagaev@gmail.com>
2016-11-12 23:30:36 -0500

    -

--- scripts/smehotron.sh
+++ scripts/smehotron.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-java -jar smehotron.jar "$@"
+# http://stackoverflow.com/a/246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+java -jar $DIR/smehotron.jar "$@"

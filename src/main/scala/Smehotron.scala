c051a74a00fd2def6c5aeb23838465650b13118d
sschwarzman <aschwarzman@yahoo.com>
2016-11-20 18:19:54 -0500

    Adjusted paths

--- src/main/scala/Smehotron.scala
+++ src/main/scala/Smehotron.scala
@@ -16,12 +16,12 @@ class Smehotron (val theRoot: Option[Path], cats: Seq[String]) extends LazyLoggi
   val jarDir = theRoot.get
   lazy val tronDir = jarDir/"schematron"
   lazy val saxonDir = jarDir/"saxon"
-  lazy val saxonClasspath = s"${saxonDir}/saxon.he.9.7.0.7.jar:${saxonDir}/resolver.jar"
+  lazy val saxonClasspath = s"${saxonDir}\\saxon.he.9.7.0.7.jar;${saxonDir}\\resolver.jar"
 
   def validate(rulesFile: String, docFile: String) =
-    for (step1 <- doStep(1, rulesFile, s"$tronDir/iso_dsdl_include.xsl");
-         step2 <- doStep(2, step1, s"$tronDir/iso_abstract_expand.xsl");
-         step3 <- doStep(3, step2, s"$tronDir/iso_svrl_for_xslt2.xsl");
+    for (step1 <- doStep(1, rulesFile, s"$tronDir\\iso_dsdl_include.xsl");
+         step2 <- doStep(2, step1, s"$tronDir\\iso_abstract_expand.xsl");
+         step3 <- doStep(3, step2, s"$tronDir\\iso_svrl_for_xslt2.xsl");
          step4 <- doStep(4, docFile, step3, ".svrl")
     ) yield step4
 

2153cbe97ee2e81866789d12d3dd141341f1c7c6
WellRedPandit <boris.dagaev@gmail.com>
2016-11-28 18:13:21 -0500

    -

--- project/Build.scala
+++ project/Build.scala
@@ -1,14 +1,12 @@
-import sbt._
 import sbt.Keys._
+import sbt._
 import sbtassembly.AssemblyPlugin.autoImport._
 
 object TronBuild extends Build {
 
   import Deps._
 
-  val repos = Seq("ERI OSS" at "http://dl.bintray.com/elderresearch/OSS")
-
-    lazy val sharedScalacSettings = Seq(
+  lazy val sharedScalacSettings = Seq(
     "-encoding", "UTF-8"
     , "-g:vars"
     , "-feature"
@@ -29,14 +27,13 @@ object TronBuild extends Build {
       version := V.tron,
       scalaVersion := V.scala,
       scalacOptions := sharedScalacSettings,
-      resolvers ++= repos,
       shellPrompt := ShellPrompt.prompt
     )
 
-    object ShellPrompt {
-      val prompt =
-        (state: State) => "%s> ".format(Project.extract(state).currentProject.id)
-    }
+  object ShellPrompt {
+    val prompt =
+      (state: State) => "%s> ".format(Project.extract(state).currentProject.id)
+  }
 
   lazy val dist = TaskKey[Unit]("dist")
 
@@ -45,25 +42,22 @@ object TronBuild extends Build {
     .settings(commonProjectSettings: _*)
     .settings(name := "smehotron",
       libraryDependencies ++=
-        compile(config ++ slf4j ++ logback ++ scalaLogging ++ commonsIo ++ scopt ++ ssc) ++
-        test(scalaCheck ++ scalaTest))
-    .settings(//test in assembly := {},
+        compile(config ++ slf4j ++ logback ++ scalaLogging ++ commonsIo ++ scopt ++ scalaXml) ++
+        test(scalaCheck ++ scalaTest),
       assemblyJarName in assembly := "smehotron.jar",
       mainClass in assembly := Some("wrp.smehotron.Smehotron"),
-      initialCommands in (Test, console) := """import wrp.smehotron._"""
-    )
-    .settings(
+      initialCommands in(Test, console) := """import wrp.smehotron._""",
       dist := {
-        import scala.collection.JavaConversions._
         import java.nio.file._
         import java.nio.file.attribute.PosixFilePermission._
+        import scala.collection.JavaConversions._
         sbt.IO.delete(new File("dist"))
         sbt.IO.copyFile(new File("target/scala-2.11/smehotron.jar"), new File("dist/smehotron.jar"), true)
         sbt.IO.copyFile(new File("scripts/smehotron.sh"), new File("dist/smehotron.sh"), true)
         sbt.IO.copyFile(new File("scripts/smehotron.bat"), new File("dist/smehotron.bat"), true)
         scala.util.Try(Files.setPosixFilePermissions(Paths.get("dist/smehotron.sh"), Set(OWNER_EXECUTE, OWNER_READ, OWNER_WRITE)))
-        sbt.IO.copyDirectory(new File("saxon"),new File("dist/saxon"),false,true)
-        sbt.IO.copyDirectory(new File("schematron"),new File("dist/schematron"),false,true)
+        sbt.IO.copyDirectory(new File("saxon"), new File("dist/saxon"), false, true)
+        sbt.IO.copyDirectory(new File("schematron"), new File("dist/schematron"), false, true)
       },
       dist <<= dist.dependsOn(assembly)
     )
--- project/Deps.scala
+++ project/Deps.scala
@@ -7,9 +7,10 @@ object V {
   val scalaCheck = "1.13.4"
   val scalaLogging = "3.5.0"
   val scalaTest = "3.0.1"
+  val scalaXml = "1.0.6"
+
   val slf4j = "1.7.21"
   val scopt = "3.5.0"
-  val ssc = "1.0.0"
   val tron = "1.0.1"
   val commonsIo = "2.5"
 }
@@ -25,7 +26,7 @@ object Deps {
   val scalaCheck = "org.scalacheck" %% "scalacheck" % V.scalaCheck
   val scalaTest = "org.scalatest" %% "scalatest" % V.scalaTest
   val scopt = "com.github.scopt" %% "scopt" % V.scopt
-  val ssc = "com.elderresearch" %% "ssc" % V.ssc
+  val scalaXml = "org.scala-lang.modules" % "scala-xml_2.11" % V.scalaXml
 
   def compile(deps: Seq[ModuleID]): Seq[ModuleID] = deps map (_ % "compile")
 

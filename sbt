fb2e7df41413ed3feaf7c582be9e784beb20121c
WellRedPandit <boris.dagaev@gmail.com>
2016-12-14 11:11:45 -0500

    -

--- sbt
+++ sbt
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
 java $SBT_OPTS -jar sbt-launch.jar "$@"
--- src/test/scala/wrp/smehotron/BasicTest.scala
+++ src/test/scala/wrp/smehotron/BasicTest.scala
@@ -13,25 +13,25 @@ class BasicTest extends FunSuite {
     val cfg = XML.loadFile(s"$localPath/basic-ok.smehotron.config.xml")
     val res = Smehotron(".", cfg).processGoModules()
     val status = (res \ "test" \ "@status").text
-    assert(status == "success")
+    assertResult("success")(status)
   }
 
   test("basic-report.xml should fail with a message") {
     val cfg = XML.loadFile(s"$localPath/basic-report.smehotron.config.xml")
     val res = Smehotron(".", cfg).processGoModules()
     val status = (res \ "test" \ "@status").text
-    assert(status == "failure")
+    assertResult("failure")(status)
     val msg = (res \ "test" \ "reports" \ "successful-report" \ "text").text
-    assert(msg == "Please inform Director of Publications about the retraction")
+    assertResult("Please inform Director of Publications about the retraction")(msg)
   }
 
   test("basic-assert.xml should fail with a message") {
     val cfg = XML.loadFile(s"$localPath/basic-assert.smehotron.config.xml")
     val res = Smehotron(".", cfg).processGoModules()
     val status = (res \ "test" \ "@status").text
-    assert(status == "failure")
+    assertResult("failure")(status)
     val msg = (res \ "test" \ "asserts" \ "failed-assert" \ "text").text
-    assert(msg == "@article-type must always be present!")
+    assertResult("@article-type must always be present!")(msg)
   }
 
 }

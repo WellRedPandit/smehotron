7f61f5c20bc928a3255e80a856c67b7a2b9e1c21
WellRedPandit <WellRedPandit@users.noreply.github.com>
2020-09-08 13:32:47 -0400

    -

--- src/main/scala/wrp/smehotron/utils/WrpXmlDiff.scala
+++ src/main/scala/wrp/smehotron/utils/WrpXmlDiff.scala
@@ -4,6 +4,8 @@ import com.github.andyglow.xml.diff.XmlOps
 import software.purpledragon.xml.compare.XmlCompare
 
 object WrpXmlDiff {
+
+  //https://github.com/stringbean/scala-xml-compare
   def compare(): Unit = {
     val doc1 = <person>
       <name>John Smith</name>
@@ -17,6 +19,7 @@ object WrpXmlDiff {
     // result1 = XmlDiffers("different text", "John Smith", "Peter Smith")
   }
 
+  // https://github.com/andyglow/scala-xml-diff
   def diff(): Unit = {
     val res = <foo/> =?= <foo/>
     println(res)
